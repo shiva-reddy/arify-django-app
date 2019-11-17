c940ab13e62bda4c3f109ff96dda25a7f23aa6e0
Shiva Reddy <skokil2@uic.edu>
2019-11-16 19:28:15 -0600

    added html forms and views

--- arify_app/views.py
+++ arify_app/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+from arify_app.models import Scene
+
+
+def index(request):
+    scenes_list = Scene.objects.all()
+    template = loader.get_template('arify_app/index.html')
+    context = {
+        'scenes_list': list(map(lambda s : s.name, scenes_list))
+    }
+    return HttpResponse(template.render(context, request))
