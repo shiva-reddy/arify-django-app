3111e44489ad101f59bf283f41818033883d87dc
Shiva Reddy <skokil2@uic.edu>
2019-11-27 18:12:19 -0600

    settings

--- arify_backend/settings.py
+++ arify_backend/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    STATIC_ROOT,
 )
 
 #  Add configuration for static files storage using whitenoise
